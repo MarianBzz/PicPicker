5ffacdf52e252455e2281f04fe139ba72b41fdaa
Mariano Bozzoletti <mbozzoletti@hotmail.com>
2022-09-07 23:13:01 -0300

    first commit, some components and screens

---
+++ screens/ImageScreen.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function ImageScreen() {
+  return (
+    <View>
+      <Text>Image Screen beibe</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
